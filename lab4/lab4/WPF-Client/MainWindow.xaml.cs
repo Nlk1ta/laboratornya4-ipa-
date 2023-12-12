60f92af6dc455a5317ffcdc46e88a7bdbab5d5b3
Nlk1ta <shmidt.n1kita@yandex.ru>
2023-12-12 23:06:01 +0700

    laba4

---
+++ lab4/lab4/WPF-Client/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Client
+{
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7083/chathub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.Reconnecting += (sender) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMassage = "Attempting to reconnect...";
+                    messages.Items.Add(newMassage);
+                });
+
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnected += (sender) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMassage = "Reconnected to the server";
+                    messages.Items.Clear();
+                    messages.Items.Add(newMassage);
+                });
+
+                return Task.CompletedTask;
+            };
+
+            connection.Closed += (sender) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMassage = "Connection closed";
+                    messages.Items.Add(newMassage);
+                    openConnection.IsEnabled= true;
+                    sendMessage.IsEnabled= false;
+                });
+
+                return Task.CompletedTask;
+            };
+        }
+
+        private async void openConnection_Click(object sender, RoutedEventArgs e)
+        {
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMassage = $"{user}: {message}";
+                    messages.Items.Add(newMassage);
+                });
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                messages.Items.Add("Connection started");
+                openConnection.IsEnabled= false;
+                sendMessage.IsEnabled= true;
+            }
+            catch(Exception ex)
+            {
+                messages.Items.Add(ex.Message);
+            }
+        }
+
+        private async void sendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage", "WPF Client", messageInput.Text);
+            }
+            catch (Exception ex)
+            {
+                messages.Items.Add(ex.Message);
+            }
+        }
+    }
+}
